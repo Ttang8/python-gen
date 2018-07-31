4bfdfd14d4267fd2eda880941fc81c68fd2c66b0
Terrence Tang <thtang28@gmail.com>
2018-07-31 16:14:40 -0700

    update

---
+++ cover_letter_gen.py
@@ -0,0 +1,52 @@
+import csv
+
+recipient = []
+application = []
+recipient_title = []
+job_position = []
+company = []
+skills = []
+why = []
+
+with open('cover_letter_gen.csv') as csv_file:
+    reader = csv.DictReader(csv_file)
+    for row in reader:
+        recipient.append(row['Recipient'])
+        recipient_title.append(row['Recipient_Title'])
+        job_position.append(row['Position'])
+        company.append(row['Company'])
+        skills.append(row['Skills'])
+        why.append(row['Why'])
+        application.append(row['Where_did_you_apply'])
+
+
+
+skill_string = skills[0]
+for i in range(len(skills)):
+    if i == 0:
+        continue
+    if i == len(skills) - 1:
+        skill_string = skill_string + ", and " + skills[i]
+    else:
+        skill_string = skill_string + ", " + skills[i]
+
+message = open('message.txt', 'w')
+dear = "Hi {},".format(recipient[0])
+line_one = "I recently applied for the {} position through {}.  I notice that you are a {} at {}. ".format(job_position[0],application[0],recipient_title[0],company[0])
+intro = "While I am not sure if you are the right person to contact, here are three reasons why I can provide value to {} and your users:".format(company[0])
+point_one = "1. {} are among my strongest technologies and I am confident in my ability to implement them as a software engineer at {} on day one.".format(skill_string,company[0])
+point_two = "2. "
+point_three = "3. "
+why_text = "{} interests me because {}.".format(company[0],why[0])
+outro = "I know you are a busy person, but I would love the opportunity to have a 10 minute chat on Skype/hangout about the role or other roles the team is recruiting for."
+message.write(dear + '\n\n')
+message.write(line_one + intro + '\n\n')
+message.write(point_one + '\n')
+message.write(point_two + '\n')
+message.write(point_three + '\n\n')
+message.write(why_text + '\n\n')
+message.write(outro + '\n')
+message.write("Thank you for your time and consideration." + '\n\n')
+message.write("Sincerely," + '\n')
+message.write("Terrence Tang")
+message.close()
