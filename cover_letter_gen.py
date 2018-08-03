e8ebf1ac33f22b1699516b5e6742d88e36c88681
Terrence Tang <thtang28@gmail.com>
2018-08-02 23:25:24 -0700

    update

--- cover_letter_gen.py
+++ cover_letter_gen.py
@@ -1,4 +1,5 @@
 import csv
+import os
 
 recipient = []
 application = []
@@ -30,13 +31,13 @@ for i in range(len(skills)):
     else:
         skill_string = skill_string + ", " + skills[i]
 
-message = open('message.txt', 'w')
+message = open('message.txt', 'w+')
 dear = "Hi {},".format(recipient[0])
 line_one = "I recently applied for the {} position through {}.  I notice that you are a {} at {}. ".format(job_position[0],application[0],recipient_title[0],company[0])
 intro = "While I am not sure if you are the right person to contact, here are three reasons why I can provide value to {} and your users:".format(company[0])
-point_one = "1. {} are among my strongest technologies and I am confident in my ability to implement them as a software engineer at {} on day one.".format(skill_string,company[0])
-point_two = "2. "
-point_three = "3. "
+point_one = "1. {} are among my strongest technologies and I am confident in my ability to implement them as a software engineer at {} and hit the ground running.".format(skill_string,company[0])
+point_two = "2. Having great teamwork, I worked closely with partners in completing daily projects during my time at App Academy, a 1000 hour, 12 week web development course."
+point_three = "3.  I pride myself on my work ethic, ability and will."
 why_text = "{} interests me because {}.".format(company[0],why[0])
 outro = "I know you are a busy person, but I would love the opportunity to have a 10 minute chat on Skype/hangout about the role or other roles the team is recruiting for."
 message.write(dear + '\n\n')
@@ -50,3 +51,6 @@ message.write("Thank you for your time and consideration." + '\n\n')
 message.write("Sincerely," + '\n')
 message.write("Terrence Tang")
 message.close()
+
+# check
+os.startfile("message.txt")
